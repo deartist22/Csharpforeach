8b0fdec0f67317400a1a29ee5ccab910b6bed5ed
deartist22 <deartist22@gmail.com>
2019-10-14 10:42:33 -0500

    Add project files.

---
+++ Csharpforeach/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Csharpforeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Done");
+            Console.ReadKey();
+        }
+    }
+}
